e069875ba9b08bbcaffb34184d16607d3ee48621
mmayyiisuay <natchasuaysaard@gmail.com>
2024-01-20 02:02:11 +0700

    new

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{vue,js,ts,jsx,tsx}",
-    'node_modules/flowbite-vue/**/*.{js,jsx,ts,tsx}',
-    'node_modules/flowbite/**/*.{js,jsx,ts,tsx}',
-  ],
+  content: [ "./index.html",
+  "./src/**/*.{vue,js,ts,jsx,tsx}",
+  "./node_modules/flowbite/**/*.js"],
   theme: {
-    extend: {
-      fontFamily: {
-        custom: ['Arial', 'sans'], // Replace 'Arial' with your desired font
-      },
-    },
+    extend: {},
   },
   plugins: [require('flowbite/plugin')],
 }
+
