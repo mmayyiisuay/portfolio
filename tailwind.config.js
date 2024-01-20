fceccc24c6c4c848d3156fec59a696035f244c42
morjor ✨ <88826980+mmayyiisuay@users.noreply.github.com>
2024-01-20 20:01:17 +0700

    Revert "change design"

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,23 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{vue,js,ts,jsx,tsx}",
-    "./node_modules/flowbite/**/*.js",
-    {relative: true,
-    transform: (content) => content.replace(/taos:/g, ''),
-    files: ['./src/*.{html,js}'],}
-  ],
+  content: [ "./index.html",
+  "./src/**/*.{vue,js,ts,jsx,tsx}",
+  "./node_modules/flowbite/**/*.js"],
   theme: {
     extend: {},
   },
-  plugins: [
-    require('taos/plugin'),  // Include the 'taos' plugin
-    require('flowbite/plugin'),
-  ],
-  safelist: [
-    '!duration-[0ms]',
-    '!delay-[0ms]',
-    'html.js :where([class*="taos:"]:not(.taos-init))'
-  ],
+  plugins: [require('flowbite/plugin')],
 }
+
