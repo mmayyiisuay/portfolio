db32b89ecab42a559c6902d7761e5cb1204867b7
mmayyiisuay <natchasuaysaard@gmail.com>
2024-01-22 21:15:05 +0700

    add tilt and experience

--- src/App.vue
+++ src/App.vue
@@ -2,14 +2,14 @@
 import About from './section/About.vue'
 import Navbar from './section/Navbar.vue'
 import Works from './section/Works.vue'
+import Contact from './section/Contact.vue'
 </script>
 
 <template >
     <Navbar/>
     <About/>
     <Works/>
-    <div class="scroll-smooth">
-    </div>
+    <Contact/>
 </template>
 
 <style scoped>
